b31ac6aff4634a7a270f0144bc9d76a5f14a6be0
Kenzo Van Brussel <114081444+YungGravity@users.noreply.github.com>
2025-01-10 05:19:28 +0100

    Add files via upload

---
+++ Album.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Album_Tracker
+{
+    public class Album
+    {
+        public string AlbumName { get; set; }
+        public string BandName { get; set; }
+        public int Rating { get; set; }
+
+        public Album(string albumname, string bandname, int rating)
+        {
+            this.AlbumName = albumname;
+            this.BandName = bandname;
+            this.Rating = rating;
+        }
+
+        public static void AddAlbum(List<Album> discog)
+        {
+            Console.Clear();
+            Console.WriteLine("----------Adding Album----------");
+            Console.WriteLine("Album name :");
+            string albumName = Console.ReadLine();
+            Console.WriteLine("Band name :");
+            string bandName = Console.ReadLine();
+            Console.WriteLine("Rating out of 10 :");
+            int rating = int.Parse(Console.ReadLine());
+
+            discog.Add(new Album(albumName, bandName, rating));
+
+            Console.Clear();
+            Console.WriteLine("Album added(Press enter to continue)");
+            Console.ReadLine();
+        }
+
+        public static void ShowAlbumList(List<Album> discog)
+        {
+            Console.Clear();
+            foreach (var album in discog)
+            {
+                Console.WriteLine($"{album.AlbumName} by {album.BandName}. Score : {album.Rating}");
+            }
+
+            Console.WriteLine("\n\n\nPress enter to go back");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Album_Tracker;
+using System.Runtime.CompilerServices;
+
+int choice = 0;
+List<Album> discog = new List<Album>();
+
+while (choice != 3)
+{
+    Console.WriteLine("What do you wanna do?\nPress 1 to add an album\nPress 2 to see the full list of listend to albums\nPress 3 to exit");
+    choice = int.Parse(Console.ReadLine());
+    if (choice == 1)
+    {
+        Album.AddAlbum(discog);
+    }
+    else if (choice == 2)
+    {
+       Album.ShowAlbumList(discog);
+    } 
+
+    Console.Clear() ;
+}
